d362b2487f73a0d99bac5759550a5c587b367d97
vinhnq12 <31241502+vinhnq12@users.noreply.github.com>
2020-03-20 14:06:14 +0700

    Update

--- src/Toggler.js
+++ src/Toggler.js
@@ -2,16 +2,25 @@ import React, { Component } from "react";
 import PubSub from "pubsub-js";
 
 export default class Toggler extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  _onClickPub = nsString => {
+    return event => {
+      PubSub.publish(nsString);
+    };
+  };
   render() {
     return (
       <div>
-        <button type="button" onClick={() => PubSub.publish("TOGGLE")}>
+        <button type="button" onClick={this._onClickPub("TOGGLE")}>
           Toggle red/blue
         </button>
-        <button type="button" onClick={() => PubSub.publish("SET PINK")}>
+        <button type="button" onClick={this._onClickPub("TOGGLE")}>
           Set pink
         </button>
-        <button type="button" onClick={() => PubSub.publish("SET YELLOW")}>
+        <button type="button" onClick={this._onClickPub("TOGGLE")}>
           Set yellow
         </button>
       </div>
